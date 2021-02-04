d60d2166e83b94c86cd515b203996d77f4d8e318
naderi8118 <70060241+naderi8118@users.noreply.github.com>
2021-02-04 13:07:52 +0330

    add commant and small edit

--- src/main.c
+++ src/main.c
@@ -10,23 +10,24 @@
 
 void main() {
     int key, x=40,y=23, bulit[MAX_Y][MAX_X],world[MAX_Y][MAX_X];
-    restB(&bulit,&world);
+    restB(&bulit,&world); // zero
     gotoxy(x,y);
-    setPaddile(&x,&y);
+    setPaddile(&x,&y); // show paddile
     while(1) {
-        if (kbhit) {
-            key = getch();
+        if (kbhit) { // if key press
+            key = getch(); // get ey
             switch(key){
-                case MAX_X: if(x > 5) x--; break;
-                case 77: if(x < MAX_X) x++; break;
-                case 32: shot(&x,&y,&bulit); break;
+                case MAX_X: if(x > 5) x--; break; // left
+                case 77: if(x < MAX_X) x++; break; // rhite
+                case 32: shot(&x,&y,&bulit); break; // space
             }
         }
         gotoxy(x,y);
-        setPaddile(&x,&y);
-        writeBulit(&bulit);
-        emShot(&bulit);
-        //emWorld(&world);
-        //drawWorld(&world);
+        setPaddile(&x,&y); // show paddile
+        writeBulit(&bulit); // show bulit
+        emShot(&bulit); // bulit
+        drawWorld(&world); // show enamy
+        //emWorld(&world); // enamy
+
     }
 }
