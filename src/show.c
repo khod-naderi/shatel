6b90e8a36cde12fe1985affec0c8301cde95a13c
naderi8118 <70060241+naderi8118@users.noreply.github.com>
2021-02-04 13:07:37 +0330

    add commant

--- src/show.c
+++ src/show.c
@@ -3,7 +3,7 @@
 
 void setPaddile(int *x,int *y){
     // draw a padding
-    clrscr();
+    clrscr(); // clear screen
     gotoxy(*x-4,*y);
     printf("<@@@@@@@>");
     gotoxy(*x-1,*y-1);
@@ -11,23 +11,23 @@ void setPaddile(int *x,int *y){
     gotoxy(*x,*y);
 }
 
-void writeBulit(int *bulit[MAX_Y][MAX_X]){
+void writeBulit(int *bulit[MAX_Y][MAX_X]){ // show the bulit
     int x,y;
-    int h=1;
-    gotoxy(1,h++);
+    gotoxy(1,1);
     for(y=0; y<MAX_Y; y++){
         for(x=0; x<MAX_X; x++)
-            printf("%c", ((bulit[y][x] == 1) ? '+' : ' '));
+			//gotoxy(x,y);
+            printf("%c", ((bulit[y][x] == 1) ? '+' : ' ')); // if bulit is say then show by + else empty
         printf("\n");
     }
 }
 
-void drawWorld(int *world[MAX_Y][MAX_X]){
+void drawWorld(int *world[MAX_Y][MAX_X]){ // show cell
     int x,y;
     gotoxy(1,1);
     for(y=0; y<MAX_Y; y++){
         for(x=0; x<MAX_X; x++){
-            printf("%c", ((world[y][x]==1) ? '#' : ' '));
+            printf("%c", ( (world[y][x]==1) ? '#' : ' ') ); // if cell is healt then show by # else empty
         }
         printf("\n");
     }
